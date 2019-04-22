31a37a65268fd1f38b6615e45c3490da60596fbb
Maritana Sampu <maritanasampu@gmail.com>
2019-04-22 11:12:39 +0300

    Add textView and sum function, reformat code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -20,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         rollButton = (Button) findViewById(R.id.rollButton);
 
         final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
-
         final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final TextView sumView = (TextView) findViewById(R.id.sumTextView);
 
         final int[] diceArray = {
                 R.drawable.dice1,
@@ -32,27 +33,22 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.dice6
         };
 
-
-
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Dicee", "The Button has been pressed");
-
                 Random randomNumberGenerator = new Random();
+                int number1 = randomNumberGenerator.nextInt(6);
+                //Log.d("Dicee", "The random number1 is: " + number1);
+                leftDice.setImageResource(diceArray[number1]);
+                int number2 = randomNumberGenerator.nextInt(6);
+                //Log.d("Dicee", "The random number2 is: " + number2);
+                rightDice.setImageResource(diceArray[number2]);
 
-                int number = randomNumberGenerator.nextInt(6);
-
-                Log.d("Dicee", "The random number is: " + number);
-
-                leftDice.setImageResource(diceArray[number]);
-
-                number = randomNumberGenerator.nextInt(6);
-
-                rightDice.setImageResource(diceArray[number]);
+                int sum = number1 + number2 + 2;
+                //Log.d("Dicee", "The sum is: " + sum);
+                sumView.setText("The sum of two dices is " + new Integer(sum).toString());
             }
         });
-
-
     }
 }
